b23eee2f2ece406bb78a8c6d16fa8a530d8db991
劉俐谷 Lucien <lucien@miniasp.com>
2017-03-03 16:14:15 +0800

    加入CaptchaSampleController * layout 增加CaptchaSampleController/Index的連結

---
+++ BotDetectCAPTCHADemo/Controllers/CaptchaSampleController.cs
@@ -0,0 +1,20 @@
+﻿using BotDetectCAPTCHADemo.Models.ViewModels;
+using System.Web.Mvc;
+
+namespace BotDetectCAPTCHADemo.Controllers
+{
+    public class CaptchaSampleController : Controller
+    {
+        // GET: CaptchaSample
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(CaptchaViewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ BotDetectCAPTCHADemo/Models/ViewModels/CaptchaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BotDetectCAPTCHADemo.Models.ViewModels
+{
+    public class CaptchaViewModel
+    {
+        /// <summary>
+        /// 驗證碼
+        /// </summary>
+        [Display(Name = "驗證碼")]
+        public string CaptchaCode { get; set; }
+    }
+}
